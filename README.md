a40b4f3c051a8b3bc1aa59ea15ee85358cf51a28
Jakub Chełchowski <jakubchelchowski@gmail.com>
2021-07-02 13:00:53 +0200

    EasyBanks

---
+++ README.md
@@ -0,0 +1,6 @@
+# EasyBanks
+## Frontend Mentor
+https://www.frontendmentor.io/challenges/easybank-landing-page-WaUhkoDN
+
+## Demo
+https://jc-easybanks.netlify.app/
---
+++ app/js/script.js
@@ -0,0 +1,23 @@
+const header = document.querySelector(".header");
+const body = document.querySelector("body");
+const btnHamburger = document.querySelector("#btnHamburger");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", () => {
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach((elem) => {
+      elem.classList.remove("fade-in");
+      elem.classList.add("fade-out");
+    });
+  } else {
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach((elem) => {
+      elem.classList.remove("fade-out");
+      elem.classList.add("fade-in");
+    });
+  }
+});
